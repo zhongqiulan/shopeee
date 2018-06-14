50c9f2019ab9b263606f7a79c42e34c8cf43c921
zhongqiulan <994700551@qq.com>
2018-06-14 08:10:30 +0800

    model.html

--- src/test/table4.vue
+++ src/test/table4.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <table class="edit-table">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="用户管理" name="first">
+      <table class="edit-table">
+      <thead>
+        <th>姓名</th><th>{{getTotalCount}}</th><th>成绩</th>
+      </thead>
+      <tbody>
+        <tr><td>bbbjjjjGGGGG44</td><td><input v-model="item1"/></td><td><i class="el-icon-remove-outline"></i></td></tr>
+        <tr><td>bbbbGGGGGGGG44</td><td><input v-model="item2"/></td><td><i class="el-icon-remove-outline"></i></td></tr>
+        <tr><td>bbbbGGGGGGGG44</td><td><input v-model="item3"/></td><td><i class="el-icon-remove-outline"></i></td></tr>
+        <tr><td>bbbbGGGGGGGG44</td><td><input v-model="item4"/></td><td><i class="el-icon-remove-outline"></i></td></tr>
+
+      </tbody>
+    </table>
+    </el-tab-pane>
+    <el-tab-pane label="配置管理" name="second">
+        <table class="edit-table">
       <thead>
         <th>姓名</th><th>{{getTotalCount}}</th><th>成绩</th>
       </thead>
@@ -10,9 +26,28 @@
         <tr><td>bbbbGGGGGGGG44</td><td><input v-model="item3"/></td><td><i class="el-icon-remove-outline"></i></td></tr>
         <tr><td>bbbbGGGGGGGG44</td><td><input v-model="item4"/></td><td><i class="el-icon-remove-outline"></i></td></tr>
 
-        
       </tbody>
     </table>
+    </el-tab-pane>
+  </el-tabs>
+
+    
+
+    <el-upload
+  class="docUploader"
+  ref='docUploader'
+  action=""
+  :limit="1"
+  :on-change="handleChange"
+  :multiple="false"
+  :auto-upload="false"
+  accept=".doc, .docx, .pdf"
+  
+  >
+  <el-button size="small" type="primary" slot="trigger" @click="clearDOC">选取文件</el-button>
+  <el-button style="margin-left:10px;" size="small" type="success" @click="handleUpload">点击推送</el-button>
+</el-upload>
+
   </div>
 </template>
 
@@ -22,6 +57,7 @@ export default {
  
   data () {
     return {
+      activeName: 'second',
     item1:1,
     item2:2,
     item3:4,
@@ -36,6 +72,29 @@ export default {
             },
   },
   methods: {
+    handleClick(tab, event) {
+        console.log(tab, event);
+      },
+     handleChange(file) {
+        this.fileType = file.name.endsWith('.pdf')?2:1
+        this.file = file
+        console.log(this.fileType);
+      },
+      clearDOC(){
+        this.$refs.docUploader.clearFiles()
+      },
+      handleUpload() {
+        const fd = new FormData()
+        fd.append('file',this.file.raw)
+        fd.append('msgType',this.msgType)
+        fd.append('fileType',this.fileType)
+        fd.append('deviceIdList',this.deviceeIdList)
+        uploadWordPDF(fd).then(data=>{
+          const result = data.data
+        })
+      }
+
+
  
 }}
 
